5aac1fb35369d9f0cbc53ebe79d89a952d724806
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2022-03-17 15:50:27 -0400

    remote collaboration

--- src/test/java/demo/Welcome.java
+++ src/test/java/demo/Welcome.java
@@ -5,5 +5,7 @@ public class Welcome {
     public static void main(String[] args) {
 
         System.out.println("Merhabalar");
+        System.out.println("Ben Lokman");
+        System.out.println("Ben Ozen");
     }
 }
